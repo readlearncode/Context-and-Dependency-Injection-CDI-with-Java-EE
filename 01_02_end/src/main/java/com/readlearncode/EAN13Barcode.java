63fc6ee6e8751037554c92d2a2fade458879cd48
Alex Theedom <alex.theedom@gmail.com>
2018-03-07 22:16:45 -0800

    Add modules and code for 01_02/01 start and end

---
+++ 01_02_end/src/main/java/com/readlearncode/EAN13Barcode.java
@@ -0,0 +1,13 @@
+package com.readlearncode;
+
+/**
+ * Source code github.com/readlearncode
+ *
+ * @author Alex Theedom www.readlearncode.com
+ * @version 1.0
+ */
+public class EAN13Barcode implements CodeGenerator {
+    public String generateCode() {
+        return "EAN13: 9780735200449";
+    }
+}
---
+++ 01_02_start/src/main/java/com/readlearncode/ProductService.java
@@ -0,0 +1,23 @@
+package com.readlearncode;
+
+/**
+ * Source code github.com/readlearncode
+ *
+ * @author Alex Theedom www.readlearncode.com
+ * @version 1.0
+ */
+public class ProductService {
+
+    private CodeGenerator codeGenerator;
+
+    ProductService(CodeGenerator codeGenerator) {
+        this.codeGenerator = codeGenerator;
+    }
+
+    public Product generateCode(Product product) {
+        String barcode = codeGenerator.generateCode();
+        product.setCode(barcode);
+        return product;
+    }
+
+}
