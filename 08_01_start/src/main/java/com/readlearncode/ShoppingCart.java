b02e4479b507f722025d4fd748bf3231639472f0
Alex Theedom <alex.theedom@gmail.com>
2018-03-13 11:40:55 -0800

    Add module 08_01

---
+++ 08_01_start/src/main/java/com/readlearncode/ShoppingCart.java
@@ -0,0 +1,17 @@
+package com.readlearncode;
+
+import javax.enterprise.context.SessionScoped;
+import java.io.Serializable;
+
+/**
+ * Source code github.com/readlearncode
+ *
+ * @author Alex Theedom www.readlearncode.com
+ * @version 1.0
+ */
+@SessionScoped
+public class ShoppingCart implements Serializable{
+
+
+
+}
